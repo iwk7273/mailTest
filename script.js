6eb737b81ab698d6278a90bf0a441125f709c0e7
iwk7273 <38845632+iwk7273@users.noreply.github.com>
2023-05-22 19:40:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@
 
 const sendDoc = (email, text) => {
   Email.send({
-    SecureToken : "FC7697E954ED2E0BFD802F48E6976CD4B06E",   
+    SecureToken : "a35b2a58-343b-4cc1-8dcf-bb744996d61e",   
     To : email,
     From : "ryutaro.iwakami@autify.com",
     Subject : "資料請求",
-    Body : text
+    Body : text,
   }).then(
     message => alert(message)
   );
@@ -23,9 +23,6 @@ btn.addEventListener('click', function() {
   const corp_name = document.getElementById("corp_name").value
   const name = document.getElementById("name").value
   const text = corp_name + '　' +  name + '様<br>'
-  
-  console.log(corp_name)
-  console.log(name)
   console.log(email)
   
   sendDoc(email,text)
