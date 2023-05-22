2a4c384baac57db7747642a48ad4dbdd29a20ec4
iwk7273 <38845632+iwk7273@users.noreply.github.com>
2023-05-22 18:49:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ btn.addEventListener('click', function() {
   const email = document.getElementById("email").value
   const corp_name = document.getElementById("corp_name").value
   const name = document.getElementById("name").value
-  const text = 'これはテストです。'
+  const text = "これはテストです。"
   
   console.log(email)
   console.log(corp_name)
